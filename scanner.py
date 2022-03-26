fdc8334b9d220a4653cab12b6ad7859c3664c378
Tony_K <kithinzi97@gmail.com>
2022-03-27 01:46:23 +0300

    Removed Constants

--- scanner.py
+++ scanner.py
@@ -4,8 +4,38 @@ import glob
 import json
 import os
 
-from .constants import *
+KILO_BYTE = 2 ** 10;
+MEGA_BYTE = 2 ** 20;
+GIGA_BYTE = 2 ** 30;
 
+CACHE_DURATION = 3600 * 7200;
+
+BATCH_SIZE = MEGA_BYTE * 256;
+
+TIME_ZONE = 10800 * int(10 ** 3);
+
+INGNORE_EVENT_TYPES = [
+    "COOKIE_INCLUSION_STATUS""CREATED_BY",
+    "CHECK_CORS_PREFLIGHT_REQUIRED",
+    "CHECK_CORS_PREFLIGHT_CACHE",
+    "COMPUTED_PRIVACY_MODE",
+    "COOKIE_INCLUSION_STATUS" ,
+    "CORS_PREFLIGHT_RESULT",
+    "CORS_PREFLIGHT_CACHED_RESULT",
+    "DELEGATE_INFO",
+    "HTTP_STREAM_JOB_BOUND_TO_REQUEST",
+    "HTTP2_SESSION_UPDATE_RECV_WINDOW",
+    "HTTP2_STREAM_UPDATE_RECV_WINDOW",
+    "HTTP2_SESSION_SEND_WINDOW_UPDATE",
+    "HTTP2_SESSION_RECV_DATA",
+    "HTTP2_SESSION_RECV_SETTING",
+    "HTTP_STREAM_JOB_CONTROLLER_BOUND" ,
+    "HTTP_STREAM_REQUEST_BOUND_TO_JOB",
+    "HTTP2_SESSION_POOL_FOUND_EXISTING_SESSION",
+    "HTTP_STREAM_REQUEST_STARTED_JOB",
+    "HTTP_STREAM_JOB_WAITING",
+    "HTTP_STREAM_JOB_CONTROLLER_PROXY_SERVER_RESOLVED",
+];
 
 file_stats = lambda file, file_path : "%.3f MB / %.3f MB  (%.3f%s) Path : %s | Last Update : %s seconds ago"%(file.tell() / MEGA_BYTE, os.stat(file_path).st_size / MEGA_BYTE, file.tell() / os.stat(file_path).st_size * 100 , "%", file_path, datetime.now().timestamp() - os.stat(file_path).st_mtime)
 
